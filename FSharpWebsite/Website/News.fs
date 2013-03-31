b5862c5c1472226e8577bc573e50a65e7993f86d
Taha Hachana <tahahachana@outlook.com>
2013-03-31 08:22:07 +0100

    generate the news section on the server side

--- FSharpWebsite/Website/News.fs
+++ FSharpWebsite/Website/News.fs
@@ -6,45 +6,61 @@ module News =
     
     module Server =
 
+        open IntelliFactory.Html
         open Mongo
 
         let newsItemData (newsItem : NewsItem) = newsItem.Title, newsItem.Url, newsItem.Summary
 
-        [<Rpc>]
+//        [<Rpc>]
         let latestNews() =
-            async {
-                let array =
-                    News.latest10()
-                    |> Seq.toArray
-                    |> Array.map newsItemData
-                return array
-            }
-
-    module Client =
-
-        open IntelliFactory.WebSharper.Html
-
-        [<JavaScriptAttribute>]
+//            async {
+//                let array =
+            News.latest10()
+            |> Seq.toArray
+            |> Array.map newsItemData
+//                return array
+//            }
+//
         let makeLi (title, url, summary) =
-            LI [
+            LI [Class "news-item"] -< [
                 A [HRef url] -< [Strong [Text title]]
                 P [Text summary]
             ]
 
-        [<JavaScript>]
         let newsList() =
-            UL [Attr.Class "unstyled"; Id "newsList"]
-            |>! OnAfterRender(fun ul ->
-                async {
-                    let! news = Server.latestNews()
-                    news
-                    |> Array.map makeLi
-                    |> Array.iter (fun x -> ul.Append x)
-                } |> Async.Start)
+            UL [Class "unstyled"; Id "newsList"] -< [
+//            |>! OnAfterRender(fun ul ->
+//                async {
+                for x in latestNews() do yield makeLi x
+            ]
+//                |> Array.iter (fun x -> ul.Append x)
+//                } |> Async.Start)
 
-        type NewsViewer() =
-
-            inherit Web.Control()
-
-            [<JavaScript>]
-            override __.Body = newsList() :> _
+//    module Client =
+//
+//        open IntelliFactory.WebSharper.Html
+//
+//        [<JavaScriptAttribute>]
+//        let makeLi (title, url, summary) =
+//            LI [
+//                A [HRef url] -< [Strong [Text title]]
+//                P [Text summary]
+//            ]
+//
+//        [<JavaScript>]
+//        let newsList() =
+//            UL [Attr.Class "unstyled"; Id "newsList"]
+//            |>! OnAfterRender(fun ul ->
+//                async {
+//                    let! news = Server.latestNews()
+//                    news
+//                    |> Array.map makeLi
+//                    |> Array.iter (fun x -> ul.Append x)
+//                } |> Async.Start)
+//
+//        type NewsViewer() =
+//
+//            inherit Web.Control()
+//
+//            [<JavaScript>]
+//            override __.Body = newsList() :> _

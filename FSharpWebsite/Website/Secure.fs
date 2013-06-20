d42c3de5bc712124930a9b7fbfc2d05e46743133
Taha Hachana <tahahachana@gmail.com>
2013-06-20 08:21:24 +0100

    uncomment secure module content

--- FSharpWebsite/Website/Secure.fs
+++ FSharpWebsite/Website/Secure.fs
@@ -2,15 +2,15 @@ namespace Website
 
 module Secure =
 
-//    let connectionString = "mongodb://readonly:readonly@ds037977.mongolab.com:37977/fsharpwebsite"
-//    let password = ""
-//    let mailgunKey = ""
-//    let mailgunDomain = ""
-//    let myEmail = ""
-//    let consKey = ""
-//    let consSecret = ""
-//    let token = ""
-//    let tokenSecret = ""
+    let connectionString = "mongodb://readonly:readonly@ds037977.mongolab.com:37977/fsharpwebsite"
+    let password = ""
+    let mailgunKey = ""
+    let mailgunDomain = ""
+    let myEmail = ""
+    let consKey = ""
+    let consSecret = ""
+    let token = ""
+    let tokenSecret = ""
 
 
 
